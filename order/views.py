079728b25968e0606980fffd3931a665a6abac7b
IsagulovUrmat <falone641234@gmail.com>
2021-04-12 19:34:40 +0600

    for beks

--- order/views.py
+++ order/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from .serializer import *
+from rest_framework import views, status
+from .models import *
 
-# Create your views here.
+
+class OrderView(views.APIView):
+
+    def get(self, request, *args, **kwargs):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": "ok!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
--- stom_crm/urls.py
+++ stom_crm/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('schedule.urls')),
-    path('', include('rates.urls'))
+    path('', include('rates.urls')),
+    path('', include('order.urls'))
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
